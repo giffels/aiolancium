19d2e2c1976610d1db6e32578c8449c53b0fdee6
Manuel Giffels <giffels@gmail.com>
2022-11-29 14:58:39 +0100

    Add upload file helper

--- aiolancium/client.py
+++ aiolancium/client.py
@@ -1,8 +1,11 @@
 from .auth import Authenticator
 from .proxy import ApiProxy
 from .resources import lancium_resources
+from .utilities.utilities import read_binary_chunks_from_file
 
 from simple_rest_client.api import API
+
+import os
 
 
 class LanciumClient(object):
@@ -12,10 +15,9 @@ class LanciumClient(object):
         self.api_proxy = ApiProxy(
             api=API(
                 api_root_url=self.api_url,
-                json_encode_body=True,
+                json_encode_body=False,
                 headers={
                     "Accept": "application/json",
-                    "Content-Type": "application/json",
                 },
                 timeout=timeout,
             ),
@@ -27,3 +29,15 @@ class LanciumClient(object):
 
     def __getattr__(self, item):
         return getattr(self.api_proxy, item)
+
+    async def upload_file_helper(self, path, source, force=True, chunk_size=32000000):
+        file_size = os.path.getsize(source)
+
+        await self.data.create_data_item(
+            path=path, source_type="file", source=source, size=file_size, force=force
+        )
+
+        for chunk_data in read_binary_chunks_from_file(
+            file_name=source, chunk_size=chunk_size
+        ):
+            await self.data.upload_data_file(path, **chunk_data)
--- aiolancium/utilities/utilities.py
+++ aiolancium/utilities/utilities.py
@@ -1,7 +1,23 @@
 from functools import partial
+from hashlib import md5
 
 
 def get_method_name(arg):
     while isinstance(arg, partial):
         arg = arg.args[0]
     return arg
+
+
+def read_binary_chunks_from_file(file_name, chunk_size):
+    with open(file_name, "rb") as f:
+        current_chunk_start = 0
+        for chunk in iter(partial(f.read, chunk_size), b""):
+            read_bytes = len(chunk)
+            checksum = md5(chunk).hexdigest()
+            headers = {
+                "Content-Length": str(read_bytes),
+                "Upload-Offset": str(current_chunk_start),
+                "Upload-Checksun": checksum,
+            }
+            yield {"headers": headers, "body": chunk}
+            current_chunk_start += read_bytes
