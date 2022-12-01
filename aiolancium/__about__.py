989df11e315110d0869461954eca1b5dcd46cdd6
Manuel Giffels <giffels@gmail.com>
2022-12-01 13:52:15 +0100

    Release version 0.2.1

--- aiolancium/__about__.py
+++ aiolancium/__about__.py
@@ -3,7 +3,7 @@ __package__ = "aiolancium"
 __summary__ = "Simplistic python asyncio REST client for the Lancium Compute REST API"
 __url__ = "https://github.com/giffels/aiolancium"
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __author__ = "Manuel Giffels"
 __email__ = "giffels@gmail.com"
 __copyright__ = "2022 %s and Contributors" % __author__
